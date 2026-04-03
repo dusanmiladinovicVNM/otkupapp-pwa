add823b0287fce864c12afb6d9b567fe5750f2b7
dusanmiladinovicVNM <162486613+dusanmiladinovicVNM@users.noreply.github.com>
2026-04-03 23:46:17 +0200

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -3,11 +3,11 @@
 // ============================================================
 window.CONFIG = {
     API_URL: 'https://script.google.com/macros/s/AKfycbyus4FqtQ9iZjHpqGiwTAgq_dkx5wqQ1x3-WriqlXIVgZ_-dNzoAvGYHejvlr6InXkrUg/exec',
-    OTKUPAC_ID: localStorage.getItem('otkupacID') || '',
-    USER_ROLE: localStorage.getItem('userRole') || '',
-    ENTITY_ID: localStorage.getItem('entityID') || '',
-    ENTITY_NAME: localStorage.getItem('entityName') || '',
-    TOKEN: localStorage.getItem('authToken') || '',
+    OTKUPAC_ID: getLs('otkupacID', ''),
+    USER_ROLE: getLs('userRole', ''),
+    ENTITY_ID: getLs('entityID', ''),
+    ENTITY_NAME: getLs('entityName', ''),
+    TOKEN: getLs('authToken', ''),
     DB_NAME: 'OtkupAppDB',
     DB_VERSION: 4,
     STORE_NAME: 'otkupi',
