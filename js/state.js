b7f9751390e21c1c3dc38ce8c70eca28efe6a75d
dusanmiladinovicVNM <162486613+dusanmiladinovicVNM@users.noreply.github.com>
2026-04-03 19:39:38 +0200

    Delete js directory

--- js/state.js
+++
@@ -1,9 +0,0 @@
-// ============================================================
-// STATE
-// ============================================================
-let db = null;
-let qrScanner = null;
-let stammdaten = { kooperanti: [], kulture: [], config: [], parcele: [], stanice: [], kupci: [], vozaci: [] };
-let selectedMera = '';
-let mgmtData = null;
-let parcelExpertOpen = {};
