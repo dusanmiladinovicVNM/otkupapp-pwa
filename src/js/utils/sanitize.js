124535adde162bb44b5947ff59629899698fe7f1
dusanmiladinovicVNM <162486613+dusanmiladinovicVNM@users.noreply.github.com>
2026-04-04 18:34:59 +0200

    Update sanitize.js

--- src/js/utils/sanitize.js
+++ src/js/utils/sanitize.js
@@ -1,1 +1,10 @@
+window.escapeHtml = function (value) {
+    if (value === null || value === undefined) return '';
 
+    return String(value)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;');
+};
