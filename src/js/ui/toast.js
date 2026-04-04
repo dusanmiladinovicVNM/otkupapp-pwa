9bf00a848d702d904a748356adf474d534eb4582
dusanmiladinovicVNM <162486613+dusanmiladinovicVNM@users.noreply.github.com>
2026-04-04 10:18:53 +0200

    Update toast.js

--- src/js/ui/toast.js
+++ src/js/ui/toast.js
@@ -1,1 +1,6 @@
-
+function showToast(msg, type = 'info') {
+    const toast = byId('toast');
+    setText(toast, msg);
+    toast.className = 'toast show ' + type;
+    setTimeout(() => { toast.className = 'toast'; }, 3000);
+}
